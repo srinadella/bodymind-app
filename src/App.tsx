f470bf8597818fb95883543b6933c655e24166a4
Sriniketh Nadella <srinikethn@gmail.com>
2026-01-03 21:00:09 -0500

    chore(lint): add TypeScript + React ESLint plugins and config; fix lint warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./app/page";
 import DashboardPage from "./app/dashboard/page";
 import ProfilesPage from "./app/profiles/page";
